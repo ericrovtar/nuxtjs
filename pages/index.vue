9956bd915147f140b09ccff56e0aed7a82de229d
Eric Rovtar <eric.rovtar@me.com>
2017-03-26 23:21:52 -0400

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="container">
+    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <h1 class="title">
+      Universal Vue.js Application Framework
+    </h1>
+    <nuxt-link class="button" to="/about">
+      About page
+    </nuxt-link>
+  </section>
+</template>
+
+<script>
+export default {
+  mounted () {
+    let files = require.context('~/posts', true, /^\.\/.*\.md$/)
+    let filenames = files.keys()
+    console.log(filenames)
+  }
+}
+</script>
+
+<style scoped>
+.title
+{
+  margin: 50px 0;
+}
+</style>
---
+++ pages/post/_id.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Hi!</h1>
+        {{ $route.params.id }}
+    </div>
+</template>
+
+<script>
+export default {
+  // asyncData ({ params, error }) {
+  //   // return axios.get(`https://jsonplaceholder.typicode.com/users/${+params.id}`)
+  //   // .then((res) => res.data)
+  //   // .catch(() => {
+  //   //   error({ message: 'User not found', statusCode: 404 })
+  //   // })
+  //   console.log(params)
+  //   return params
+  // }
+}
+</script>
+
+<style scoped></style>
